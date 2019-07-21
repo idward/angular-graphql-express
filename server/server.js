f07ff7c3d73e5529e24e873846d9fdff7734d672
idward <idward@vip.sina.com>
2019-07-22 07:36:18 +0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { ApolloServer, gql } from "apollo-server-express"
+
+const app = express()
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!',
+    },
+};
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+apolloServer.applyMiddleware({ app })
+
+app.listen(4000, () => {
+    console.log('Server is listening on port 4000');
+})
