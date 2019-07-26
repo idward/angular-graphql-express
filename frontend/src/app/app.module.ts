92b6c80d00af2c74bd3223cf1845f6f986d9b007
idward <idward@vip.sina.com>
2019-07-26 08:41:18 +0800

    update files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './pages/course-list/course-list.component';
+import { CourseItemComponent } from './pages/course-item/course-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    CourseItemComponent
   ],
   imports: [
     BrowserModule,
